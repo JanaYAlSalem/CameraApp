77060b737fc867b1cbc5c4539fdb43b3c0c1587c
Jana AlSalem <92260239+JanaYAlSalem@users.noreply.github.com>
2024-06-29 10:48:07 +0300

    comment

--- app/src/main/java/com/example/cameraapp/MainActivity.kt
+++ app/src/main/java/com/example/cameraapp/MainActivity.kt
@@ -22,7 +22,8 @@ class MainActivity : AppCompatActivity() {
     private val REQUEST_IMAGE_CAPTURE = 1
     private val REQUEST_PICK_IMAGE = 2
 
-    
+
+    // test
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
